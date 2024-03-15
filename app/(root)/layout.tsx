b37c5d6c0d4c7e46787bd6f895bc2f6c00eca9bb
hisham qandil <hishamqandil@hishams-MacBook-Pro.local>
2024-03-15 00:35:51 -0700

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,22 @@
+import MobileNav from '@/components/share/MobileNav'
+import Sidebar from '@/components/share/Sidebar'
+import { Toaster } from '../../src/components/ui/toaster'
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <Sidebar />
+      <MobileNav />
+
+      <div className="root-container">
+        <div className="wrapper">
+          {children}
+        </div>
+      </div>
+      
+      <Toaster />
+    </main>
+  )
+}
+
+export default Layout
